05c2617e6cfe39d4d739303c7953daba154a87be
Jmats17 <Justin@Justins-MacBook-Air.local>
2015-07-20 10:48:44 -0700

    main page working

---
+++ GroupAlarm/PageContentViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PageContentViewController.swift
+//  GroupAlarm
+//
+//  Created by Justin Matsnev on 7/17/15.
+//  Copyright (c) 2015 Justin Matsnev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Parse
+import Bolts
+
+class PageContentViewController : UIViewController {
+    
+}
